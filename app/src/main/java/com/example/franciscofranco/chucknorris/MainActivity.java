3fe8773fbc7233447561018b090b9d33670e85fa
Franco <frfranco@uci.edu>
2016-08-11 16:05:20 -0700

    implemented personal jokes

--- app/src/main/java/com/example/franciscofranco/chucknorris/MainActivity.java
+++ app/src/main/java/com/example/franciscofranco/chucknorris/MainActivity.java
@@ -1,16 +1,22 @@
 package com.example.franciscofranco.chucknorris;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -19,12 +25,17 @@ import org.json.JSONObject;
 import java.util.LinkedList;
 
 public class MainActivity extends AppCompatActivity {
+
     private ListView listView;
     private SeekBar seekBar;
     private TextView textView;
     private LinkedList<String> jokes;
     private ArrayAdapter<String> itemsAdapter;
     private int jokeCount = 1;
+    private static final String PREFS = "prefs";
+    private static final String PREF_NAME = "name";
+    SharedPreferences mSharedPreferences;
+    private String customName = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,6 +68,7 @@ public class MainActivity extends AppCompatActivity {
 
         itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, jokes);
         listView.setAdapter(itemsAdapter);
+        displayWelcome();
 
     }
 
@@ -67,7 +79,11 @@ public class MainActivity extends AppCompatActivity {
     public void makeAsyncRequest() {
         String url = "http://api.icndb.com/jokes/random/" + Integer.toString(jokeCount);
         AsyncHttpClient client = new AsyncHttpClient();
-        client.get(url, new JsonHttpResponseHandler() {
+        RequestParams params = new RequestParams();
+        String[] name = customName.split(" ");
+        params.put("firstName", name[0]);
+        params.put("lastName", name[1]);
+        client.get(url, params, new JsonHttpResponseHandler() {
             @Override
             public void onSuccess(JSONObject response) {
                 jokes.clear();
@@ -93,6 +109,61 @@ public class MainActivity extends AppCompatActivity {
 
         } catch (JSONException e) {
             e.printStackTrace();
+        }
+    }
+
+    public void displayWelcome() {
+
+        // Access the device's key-value storage
+        mSharedPreferences = getSharedPreferences(PREFS, MODE_PRIVATE);
+
+        // Read the user's name,
+        // or an empty string if nothing found
+        String name = mSharedPreferences.getString(PREF_NAME, "");
+        customName = name;
+        if (name.length() > 0) {
+
+            // If the name is valid, display a Toast welcoming them
+            Toast.makeText(this, "Welcome back, " + name + "!", Toast.LENGTH_LONG).show();
+        }  else {
+
+            // otherwise, show a dialog to ask for their name
+            AlertDialog.Builder alert = new AlertDialog.Builder(this);
+            alert.setTitle("Hello!");
+            alert.setMessage("What is your name?");
+
+            // Create EditText for entry
+            final EditText input = new EditText(this);
+            alert.setView(input);
+
+            // Make an "OK" button to save the name
+            alert.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+
+                public void onClick(DialogInterface dialog, int whichButton) {
+
+                    // Grab the EditText's input
+                    String inputName = input.getText().toString();
+
+                    // Put it into memory (don't forget to commit!)
+                    SharedPreferences.Editor e = mSharedPreferences.edit();
+                    e.putString(PREF_NAME, inputName);
+                    e.commit();
+
+                    customName = inputName;
+
+                    // Welcome the new user
+                    Toast.makeText(getApplicationContext(), "Welcome, " + inputName + "!", Toast.LENGTH_LONG).show();
+                }
+            });
+
+            // Make a "Cancel" button
+            // that simply dismisses the alert
+            alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+
+                public void onClick(DialogInterface dialog, int whichButton) {}
+            });
+
+            alert.show();
         }
     }
 
